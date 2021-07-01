736f256f8e007841ce92d3d10cec8b1c4f12ab4a
Maneesha a pillai <77294239+Maneesha9801@users.noreply.github.com>
2021-07-01 11:25:11 +0400

    Update swap.c

---
+++ swap.c
@@ -0,0 +1,26 @@
+void swap_max( int arr[], int l, int n){
+ int swap =0;
+ int index =0;
+ int a=0;
+ for( int i =n;i<l;i++){
+ if(a<arr[i]){
+ a=arr[i];
+ index=i ;
+ 
+ }
+ }
+ 
+ swap=arr[index];
+ arr[index]=arr[n];
+ arr[n] = swap;
+ }
+ 
+ 
+ Void ssort( int arr[], int l)
+ {
+ for(int i=0;i<=l-1;i++)
+ {
+ swap_max(arr,l,i)
+ }
+ }
+ 
